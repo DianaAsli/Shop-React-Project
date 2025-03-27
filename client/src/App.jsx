008a2ff7959223c61ab147950335ca974bd3c36d
DianaAsli <dianaasli201001@gmail.com>
2025-03-27 22:36:12 +0200

    Implement Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router';
 import Home from './components/home/Home';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />Д
       </Routes>
-
+      <Footer/>
     </div>
   )
 }
