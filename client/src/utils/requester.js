78bb31b458dff963d13aa8e1d17eb5172a4acd66
DianaAsli <dianaasli201001@gmail.com>
2025-03-29 16:49:54 +0200

    Add reusable HTTP request function

---
+++ client/src/utils/requester.js
@@ -0,0 +1,17 @@
+export const request = async (method, url, data) => {
+    const options = {
+        method,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    if (data && method !== 'GET') {
+        options.body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, options);
+    const result = await response.json();
+
+    return result;
+}
